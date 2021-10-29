c52d1bb703a10a84eaa8ca3e6c84181fe535de38
Eliazer Braun <ebraun@adorama.com>
2021-10-29 10:39:42 -0400

    updated README and formated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ## color-swatch-gradient
+
+
+### Description
+![image-swatches](./images/image-swatches.png?raw=true)
+
+this package allows you to generate multi color swatches on the fly 
+via 
+```
+colorSwatchGradient(["red", "white", "blue"]) // outputs: '-45deg, red 0%, red 33%, white 33%, white 66%, blue 66%, blue 99%'
+// 
+```
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const generateColorGradient = colors => {
     const incrementBy = Math.floor(100 / colors.length)
     return `-45deg, ${colors
         .map((color, index) => {
-            return `${color} ${incrementBy * index}%, ${color} ${incrementBy * (index + 1)}%`
+            return `${color} ${incrementBy * index}%, ${color} ${incrementBy * (index + 1)}% `
         })
         .join(',')
         .replace(/,+$/, '')}`
